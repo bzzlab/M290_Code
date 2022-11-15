cb381326d9f4ab296bb2afa993a8400315d7ff59
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2022-11-15 18:11:55 +0100

    Initial version

---
+++ 08_Webapp_Backend_mit_Datenbank_verbinden/02_Exercises_Loes-1/contact-server/customer/api.js
@@ -0,0 +1,32 @@
+const customer = require("./controller.js");
+module.exports = app => {
+  // Create a new Customer
+  app.post("/customer", customer.create);
+
+  // Retrieve all Customers
+  app.get("/customers", customer.findAll);
+
+  //Aufgabe: REST-API, um EINEN Kunden anhand der ID auszugeben
+  //--Begin
+  // Retrieve a single Customer with id
+  app.get("/customer/:id",customer.findOne)
+  //--End
+
+  // Update a Customer with customerId
+  app.put("/customer/:id", customer.update);
+
+
+  //Aufgabe: REST-API, um EINEN Kunden anhand der ID zu löschen
+  //--Begin
+  // Delete a Customer with customerId
+  app.delete("/customer/:id", customer.remove);
+  //--End
+
+
+  //Aufgabe: REST-API, um alle Kunden zu löschen
+  //--Begin
+  // delete all customers
+
+  //--End
+
+};
